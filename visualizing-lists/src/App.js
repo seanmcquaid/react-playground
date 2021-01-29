7d66fd5db12ccc426ae4f5c3b380aa4d81889fe2
seanmcquaid <sean.mcquaid2@verizon.net>
2021-01-28 22:37:36 -0500

    Definitely prefer virtualized way more

--- visualizing-lists/src/App.js
+++ visualizing-lists/src/App.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 import H1 from './components/H1';
 import Virtualized from './containers/Virtualized';
+import Window from './containers/Window';
 
 const App = () => (
   <PageContainer>
@@ -9,6 +10,7 @@ const App = () => (
     </Header>
     <Main>
       <Virtualized />
+      <Window />
     </Main>
   </PageContainer>
 );
--- visualizing-lists/src/containers/Window/index.js
+++ visualizing-lists/src/containers/Window/index.js
@@ -1,3 +1,66 @@
-const Window = () => {};
+import { useCallback, useEffect, useReducer } from 'react';
+import { VariableSizeList as List } from 'react-window';
+import jokesService from '../../api/jokesService';
+
+const initialState = {
+  data: [],
+  isLoading: false,
+  error: null,
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'LOADING':
+      return {
+        ...state,
+        isLoading: true,
+      };
+    case 'SUCCESS':
+      return {
+        ...state,
+        isLoading: false,
+        data: action.payload.data,
+      };
+    case 'ERROR':
+      return {
+        ...state,
+        isLoading: false,
+        error: action.payload.error,
+      };
+    default:
+      return state;
+  }
+};
+
+const Window = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    dispatch({ type: 'LOADING' });
+
+    jokesService
+      .getCategories()
+      .then(({ data }) => {
+        dispatch({ type: 'SUCCESS', payload: { data } });
+      })
+      .catch((error) => {
+        dispatch({ type: 'ERROR', payload: { error } });
+      });
+  }, []);
+
+  const Row = ({ index, style }) => <div style={style}>{state[index]}</div>;
+
+  return (
+    <List
+      className='List'
+      height={600}
+      itemCount={state.data.length}
+      itemSize={() => 35}
+      width={300}
+    >
+      {Row}
+    </List>
+  );
+};
 
 export default Window;
